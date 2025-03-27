dc56d52c4aa41dfff958c159784439125c42a1e6
Felipe Lins <felipeplinsesilva@gmail.com>
2025-03-26 22:55:50 -0300

    s

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
-import fs from 'fs'
-
-function copyRedirects() {
-  return {
-    name: 'copy-redirects',
-    closeBundle() {
-      fs.copyFileSync('public/_redirects', 'dist/_redirects')
-    }
-  }
-}
 
 export default defineConfig({
-  plugins: [react(), copyRedirects()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
+  plugins: [react()]
 })
