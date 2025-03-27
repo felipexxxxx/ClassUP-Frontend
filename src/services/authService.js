b5b50b60ab2d9f8ebae69457fb8ca5f6e09722ba
Felipe Lins <felipeplinsesilva@gmail.com>
2025-03-27 13:08:49 -0300

    ajuste de recebimento do dto

--- src/services/authService.js
+++ src/services/authService.js
@@ -14,11 +14,11 @@ export async function logoutUsuario() {
   await api.post("/user/logout");
 }
 
-export async function enviarCodigoEmail(novoEmail) {
-  return api.post("/user/enviar-email", { novoEmail });
+export async function enviarCodigoEmail(email) {
+  return api.post("/user/enviar-email", { email });
 }
 
-export async function redefinirSenha(email, codigoAutenticacao, novaSenha) {
+export async function redefinirSenha(codigoAutenticacao, novaSenha) {
   return api.put("/user/redefinir-senha", { novaSenha, codigoAutenticacao});
 }
 
