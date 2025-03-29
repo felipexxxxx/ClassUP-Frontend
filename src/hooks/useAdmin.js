28107239378383f80e7f7f615ec76ad002e65b4c
Felipe Lins <felipeplinsesilva@gmail.com>
2025-03-29 16:43:16 -0300

    teste 2

--- src/hooks/useAdmin.js
+++ src/hooks/useAdmin.js
@@ -1,56 +1,39 @@
-import { useState } from "react";
-import { importarUsuarios } from "../services/adminService";
+const importar = async (formData) => {
+  setMensagem("");
+  setSucesso(false);
+  setCarregando(true);
 
-export default function useAdmin() {
-  const [mensagem, setMensagem] = useState("");
-  const [sucesso, setSucesso] = useState(false);
-  const [carregando, setCarregando] = useState(false);
+  try {
+    const response = await fetch("https://classup-python-converter-production.up.railway.app/converterJson", {
+      method: "POST",
+      body: formData,
+    });
 
-  const importar = async (formData) => {
-    setMensagem("");
-    setSucesso(false);
-    setCarregando(true);
+    const json = await response.json();
 
-    try {
-      const response = await fetch("https://classup-python-converter-production.up.railway.app/converterJson", {
-        method: "POST",
-        body: formData,
-      });
-
-      if (!response.ok) {
-        const erro = await response.json();
-        throw new Error(erro.erro || "Erro ao converter o arquivo.");
-      }
-
-      const usuarios = await response.json();
-      console.log("Usuários recebidos do Flask:", usuarios); 
-      await importarUsuarios(usuarios);
-     
-
-      setMensagem("✅ Usuários importados com sucesso!");
-      setSucesso(true);
-    } catch (error) {
-      console.error("Erro na importação:", error);
-
-      if (error.response && error.response.data) {
-        const msg = typeof error.response.data === "string"
-          ? error.response.data
-          : error.response.data.erro || "Erro ao importar usuários.";
-        setMensagem(`⚠️ ${msg}`);
-      } else {
-        setMensagem(`❌ ${error.message || "Erro inesperado ao importar usuários."}`);
-      }
-
-      setSucesso(false);
-    } finally {
-      setCarregando(false);
+    if (!response.ok) {
+      throw new Error(json.erro || "Erro ao converter o arquivo.");
     }
-  };
 
-  return {
-    importar,
-    mensagem,
-    sucesso,
-    carregando,
-  };
-}
+    console.log("Usuários recebidos do Flask:", json);
+    await importarUsuarios(json);
+
+    setMensagem("✅ Usuários importados com sucesso!");
+    setSucesso(true);
+  } catch (error) {
+    console.error("Erro na importação:", error);
+
+    if (error.response && error.response.data) {
+      const msg = typeof error.response.data === "string"
+        ? error.response.data
+        : error.response.data.erro || "Erro ao importar usuários.";
+      setMensagem(`⚠️ ${msg}`);
+    } else {
+      setMensagem(`❌ ${error.message || "Erro inesperado ao importar usuários."}`);
+    }
+
+    setSucesso(false);
+  } finally {
+    setCarregando(false);
+  }
+};
